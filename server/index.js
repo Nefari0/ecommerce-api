6f5f1ed4649fdf215442352b089b7c79f6425002
Chris deMontigny <chrisdemontigny@yahoo.com>
2020-12-19 10:43:26 -0700

    ecommerce-api

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const getProducts = require('./getProducts');
+
+// const getProducts = require('./getProducts');
 const getProduct = require('./getProduct');
-const products = require('../products.json')
+const products = require('../products.json');
+const getProducts = require('./getProducts');
 
 
 const app = express();
-const port = 4444;
+const port = 3001;
 
 
-app.get('/api/products', getProducts)
-app.get('./api/products', getProducts);
+// app.get('/api/products', (req,res) => {
+//     res.status(200).send(products)
+// })
+app.get('/api/products', getProducts);
+app.get('/api/products/:id', getProduct)
 
-app.get('./api/products/:id', getProduct);
+// app.get('./api/products/:id', getProduct);
 
 app.listen(port, () => {
     console.log(`server listening on port: ${port}`);
